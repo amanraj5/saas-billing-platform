a53739159366465e163b1ec76239f6c74b67bb00
Aman Raj <amanrajsingh806@gmail.com>
2026-07-25 19:34:14 +0530

    initial changes done required for buildin application

--- tenant-service/src/main/java/com/example/demo/repository/TenantRepository.java
+++ tenant-service/src/main/java/com/example/demo/repository/TenantRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Tenant;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TenantRepository extends MongoRepository<Tenant, String> {
     List<Tenant> findByTenantId(String tenantId);
-
 }
